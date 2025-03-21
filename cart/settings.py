0b90e809cbd33fb3e391d0d561bdc026109d3fe9
jeff1168tw <jeff1168tw@yahoo.com>
2025-03-21 21:48:37 +0800

    Update settings.py
    add os.path.join(BASE_DIR, 'static')

--- cart/settings.py
+++ cart/settings.py
@@ -152,7 +152,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    #BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
