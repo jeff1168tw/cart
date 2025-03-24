b028cd36b78a0ccac1622becf0b72e04afc5cb48
陳0夫 <jeff1168tw@yahoo.com>
2025-03-24 19:54:29 +0800

    Update settings.py

--- cart/settings.py
+++ cart/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-MEDIA_URL = '/static/'
+#MEDIA_URL = '/static/'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
