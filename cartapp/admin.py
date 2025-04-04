88d2becb0751bbdcce123baa46fd4d8f833dccfc
陳0夫 <jeff1168tw@yahoo.com>
2025-04-04 19:12:24 +0800

    Update admin.py
    20240404 add temperature_db

--- cartapp/admin.py
+++ cartapp/admin.py
@@ -4,3 +4,4 @@ from cartapp import models
 admin.site.register(models.ProductModel)
 admin.site.register(models.OrdersModel)
 admin.site.register(models.DetailModel)
+admin.site.register(models.temperature_db)
